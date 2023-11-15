c9acd6b10763c21f2d24016322d0ef2423b13742
Camille Kirstine <charlieslarsson@icloud.com>
2023-11-15 22:44:26 +0100

    Front connected to backend
    Frontpage is now connected to backend

--- Kiddo/src/app/pages/front/front.page.ts
+++ Kiddo/src/app/pages/front/front.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivityCard } from 'src/app/types/ActivityCard';
 import { ActivitiesService } from 'src/app/services/activitiesservice.service';
 
+
 @Component({
   selector: 'app-front',
   templateUrl: './front.page.html',
@@ -16,7 +17,26 @@ export class FrontPage implements OnInit {
   }
 
   ngOnInit() {
+    this.fetchActivities();
   }
+
+  fetchActivities() {
+    this.activitiesService.getActivities().subscribe(
+      activities => {
+        // Sort activities based on createdAt in descending order
+        this.originalActivities = activities.sort((a, b) => {
+          const createdAtA = new Date(a.createdAt).getTime();
+          const createdAtB = new Date(b.createdAt).getTime();
+          return createdAtB - createdAtA;
+        });
+      },
+      error => {
+        console.error('Failed to fetch activities:', error);
+        // Handle the error here
+      }
+    );
+  }
+  
 
 
 }
