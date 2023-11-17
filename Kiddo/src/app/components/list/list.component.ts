d5d15122a2d28367653b94be4665ad790ea7a130
Mark <markphillip1800@gmail.com>
2023-11-17 03:29:04 +0100

    delete button (not working yet)

--- Kiddo/src/app/components/list/list.component.ts
+++ Kiddo/src/app/components/list/list.component.ts
@@ -2,12 +2,17 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivityCard } from '../../types/ActivityCard';
 import { ActivitiesService } from 'src/app/services/activitiesservice.service';
 
+
+import { ActivatedRoute, Router } from '@angular/router';
+import { ActivityService } from 'src/app/services/activity.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
-  providers: [ActivitiesService]
 })
+
 export class ListComponent  implements OnInit {
   @Input() activity: ActivityCard = {
     id: 0,
@@ -29,10 +34,35 @@ export class ListComponent  implements OnInit {
 
   @Input() showImage: boolean = true;
 
-  constructor(private activitiesService: ActivitiesService) { }
+  constructor(
+    private activitiesService: ActivitiesService,
+    private http: HttpClient,
+
+    private activityService: ActivityService,
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit() {
     this.activitiesService.fetchActivities();
+  }
+
+  makeHttpRequest(): void {
+    const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+    const now = new Date();
+
+    this.http.delete('http://localhost:8080/activities/delete/:id').subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  onDelete(): void {
+    this.makeHttpRequest();  
   }
 
 }
